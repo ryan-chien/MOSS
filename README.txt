bf158bf01265e18e24bd182e96ad515ec513b1ee
rychien-official <rvc5634@psu.edu>
2019-12-16 20:40:05 -0500

    Cleanup of "MOSS.py" and updated readme.

--- README.txt
+++ README.txt
@@ -8,4 +8,31 @@ MOSS utilizes the absolute value constraint to enforce the all-different rule ne
 For details of the absolute value constraint, see Gurobi Modeling, "Non Convex Case", page 9:
 <https://www.gurobi.com/pdfs/user-events/2017-frankfurt/Modeling-2.pdf>
 
+As of current (12/16/2019), MOSS satisfies the all-different constraints within each row. However, neither the
+all-different constraints within each column nor the all-different constraints within each nonlet are satisfied.
+Thus, satisfaction of these constraints are the subject of ongoing study. See below for an example input board, and
+MOSS solution (i.e. output).
+
+    Here is the input Sudoku board:
+     [  [0, 0, 0, 0, 0, 4, 0, 2, 8],
+        [4, 0, 6, 0, 0, 0, 0, 0, 5],
+        [1, 0, 0, 0, 3, 0, 6, 0, 0],
+        [0, 0, 0, 3, 0, 1, 0, 0, 0],
+        [0, 8, 7, 0, 0, 0, 1, 4, 0],
+        [0, 0, 0, 7, 0, 9, 0, 0, 0],
+        [0, 0, 2, 0, 1, 0, 0, 0, 3],
+        [9, 0, 0, 0, 0, 0, 5, 0, 7],
+        [6, 7, 0, 4, 0, 0, 0, 0, 0]  ]
+
+    And the output provided by MOSS:
+     [  [6. 3. 5. 7. 1. 4. 9. 2. 8.]
+        [4. 1. 6. 2. 8. 9. 3. 7. 5.]
+        [1. 2. 7. 8. 3. 4. 6. 9. 5.]
+        [2. 6. 8. 3. 7. 1. 5. 9. 4.]
+        [9. 8. 7. 6. 5. 3. 1. 4. 2.]
+        [4. 2. 8. 7. 5. 9. 1. 3. 6.]
+        [4. 8. 2. 6. 1. 9. 7. 5. 3.]
+        [9. 8. 1. 2. 6. 4. 5. 3. 7.]
+        [6. 7. 1. 4. 9. 2. 8. 3. 5.]  ]
+
 Thanks to Seth W for the inspiration for this toy problem.
--- source/MOSS.py
+++ source/MOSS.py
@@ -24,30 +24,6 @@ initial_board = np.array([
     [6, 7, 0, 4, 0, 0, 0, 0, 0]
 ])
 
-initial_board = np.array([
-    [0, 0, 0, 7, 0, 4, 0, 0, 5],
-    [0, 2, 0, 0, 1, 0, 0, 7, 0],
-    [0, 0, 0, 0, 8, 0, 0, 0, 2],
-    [0, 9, 0, 0, 0, 6, 2, 5, 0],
-    [6, 0, 0, 0, 7, 0, 0, 0, 8],
-    [0, 5, 3, 2, 0, 0, 0, 1, 0],
-    [4, 0, 0, 0, 9, 0, 0, 0, 0],
-    [0, 3, 0, 0, 6, 0, 0, 9, 0],
-    [2, 0, 0, 4, 0, 7, 0, 0, 0]
-])
-
-initial_board = np.array([
-    [0, 9, 3, 0, 0, 4, 5, 6, 0],
-    [0, 6, 0, 0, 0, 3, 1, 4, 0],
-    [0, 0, 4, 6, 0, 8, 3, 0, 9],
-    [9, 8, 1, 3, 4, 5, 0, 0, 0],
-    [3, 4, 7, 2, 8, 6, 9, 5, 1],
-    [6, 5, 2, 0, 7, 0, 4, 8, 3],
-    [4, 0, 6, 0, 0, 2, 8, 9, 0],
-    [0, 0, 0, 4, 0, 0, 0, 1, 0],
-    [0, 2, 9, 8, 0, 0, 0, 3, 4]
-])
-
 # Create solver
 print("(1) Initializing optimization model...")
 solver = pywraplp.Solver(
@@ -77,7 +53,6 @@ for i in range(0, 9):
             initial_board_constraints[i].append(
                 'init_x'+str(i)+str(j)+'==NONE'
             )
-
 
 ## Sum of Rows Must Equal 45
 constraint_a = [[]] * 9
@@ -210,125 +185,6 @@ for i in range(0, constraint_var_t_iforallj.__len__()):
     constraint_zpn_iforallj.append(constraint_zpn_i)
     constraint_yp_iforallj.append(constraint_yp_i)
     constraint_yn_iforallj.append(constraint_yn_i)
-
-## Absolute Value Constraints, j for all i
-### part 4
-constraint_var_u_jforalli = []
-constraint_u_jforalli = []
-for i in range(0, 9):
-    constraint_var_u_i = []
-    constraint_u_i = []
-    for k in range(0, 8):
-        constraint_var_u_j = []
-        constraint_u_j = []
-        for j in range(0+k, 8):
-            #### define variable t, integer, range of -99 to 99
-            #### one t for each unique objective variable x pair
-            constraint_var_u_j.append(
-                solver.IntVar(
-                    -99, 99, 'u'+str(j)+str(i)+str(j+1)+str(i)))
-            #### constraint, e.g. t0001 = x00 - x01 ... t0002 = x00 - x02
-            constraint_u_j.append(
-                solver.Add(
-                    constraint_var_u_j[j-k]
-                    - objective_vars[k][i]
-                    + objective_vars[j+1][i]
-                    == 0,
-                    'u'+str(i)+str(k)+str(i)+str(j+1)))
-            print("Constraint: " + constraint_var_u_j[j-k].name() + ' - ' + objective_vars[k][i].name()
-                    + ' + ' + objective_vars[j+1][i].name() + ' = ' + ' 0 ')
-        constraint_var_u_i.append(constraint_var_u_j)
-        constraint_u_i.append(constraint_u_j)
-    constraint_var_u_jforalli.append(constraint_var_u_i)
-    constraint_u_jforalli.append(constraint_u_i)
-
-### part 5
-constraint_var_q_jforalli = []
-constraint_var_o_jforalli = []
-constraint_var_a_jforalli = []
-constraint_var_b_jforalli = []
-for row in constraint_var_t_iforallj:
-    constraint_var_q_i = []
-    constraint_var_o_i = []
-    constraint_var_a_i = []
-    constraint_var_b_i = []
-    for column in row:
-        constraint_var_q_j = []
-        constraint_var_o_j = []
-        constraint_var_a_j = []
-        constraint_var_b_j = []
-        for variable_t in column:
-            constraint_var_q_j.append(
-                solver.IntVar(
-                    0, 99, 'q'+variable_t.name()[1:5]))
-            constraint_var_o_j.append(
-                solver.IntVar(
-                    0, 99, 'o'+variable_t.name()[1:5]))
-            constraint_var_a_j.append(
-                solver.IntVar(
-                    1, 99, 'a'+variable_t.name()[1:5])) # Note that z must be greater than or equal one
-            constraint_var_b_j.append(
-                solver.BoolVar(
-                    'b'+variable_t.name()[1:5]))
-        constraint_var_q_i.append(constraint_var_q_j)
-        constraint_var_o_i.append(constraint_var_o_j)
-        constraint_var_a_i.append(constraint_var_a_j)
-        constraint_var_b_i.append(constraint_var_b_j)
-    constraint_var_q_jforalli.append(constraint_var_q_i)
-    constraint_var_o_jforalli.append(constraint_var_o_i)
-    constraint_var_a_jforalli.append(constraint_var_a_i)
-    constraint_var_b_jforalli.append(constraint_var_b_i)
-
-### absolute value constraints, part 6
-constraint_upn_iforallj = []
-constraint_apn_iforallj = []
-constraint_bp_iforallj = []
-constraint_bn_iforallj = []
-for i in range(0, constraint_var_u_jforalli.__len__()):
-    constraint_upn_i = []
-    constraint_apn_i = []
-    constraint_bp_i = []
-    constraint_bn_i = []
-    for j in range(0, constraint_var_u_jforalli[i].__len__()):
-        constraint_upn_j = []
-        constraint_apn_j = []
-        constraint_bp_j = []
-        constraint_bn_j = []
-        for k in range(0, constraint_var_u_jforalli[i][j].__len__()):
-            constraint_upn_j.append(
-                solver.Add(
-                    constraint_var_u_jforalli[i][j][k]
-                    - constraint_var_q_jforalli[i][j][k]
-                    + constraint_var_o_jforalli[i][j][k]
-                    == 0,
-                    'upn'+constraint_var_u_jforalli[i][j][k].name()[1:5]))
-            constraint_apn_j.append(
-                solver.Add(
-                    constraint_var_a_jforalli[i][j][k]
-                    - constraint_var_q_jforalli[i][j][k]
-                    - constraint_var_o_jforalli[i][j][k]
-                    == 0,
-                    'apn'+constraint_var_a_jforalli[i][j][k].name()[1:5]))
-            constraint_bp_j.append(
-                solver.Add(
-                    constraint_var_b_jforalli[i][j][k]
-                    - 99*constraint_var_a_jforalli[i][j][k]
-                    <= 0,
-                    'bp'+constraint_var_q_jforalli[i][j][k].name()[1:5]))
-            constraint_bn_j.append(
-                solver.Add(
-                    constraint_var_o_jforalli[i][j][k]
-                    + 99*constraint_var_b_jforalli[i][j][k]
-                    <= 99,
-                    'bn' + constraint_var_o_jforalli[i][j][k].name()[1:5]))
-        constraint_upn_i.append(constraint_upn_j)
-        constraint_apn_i.append(constraint_apn_j)
-        constraint_bp_i.append(constraint_bp_j)
-        constraint_bn_i.append(constraint_bn_j)
-    constraint_upn_iforallj.append(constraint_upn_i)
-    constraint_apn_iforallj.append(constraint_apn_i)
-    constraint_bp_iforallj.append(constraint_bp_i)
-    constraint_bn_iforallj.append(constraint_bn_i)
 
 # Solution
 solver.SetTimeLimit(120000)
